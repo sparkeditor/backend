531b3ddaf2ac64371e821533f6d3fe37e4f72bf8
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-02-04 12:04:52 -0700

    Promisify fileIOTest

--- test/fileIOTest.js
+++ test/fileIOTest.js
@@ -1,6 +1,9 @@
+const Promise = require("bluebird");
 const fs = require("fs");
 const expect = require("chai").expect;
 const mock = require("mock-fs");
+
+Promise.promisifyAll(fs);
 
 const fileIO = require("../lib/fileIO");
 
@@ -19,152 +22,103 @@ describe("FileIO", function() {
         mock.restore();
     });
 
-    it("reads from an existing file asynchronously", function(done) {
-        fileIO.read("test/dir1/file1", function(error, data) {
-            expect(error).to.not.exist;
-            expect(data).to.equal("This is a test file.");
-            done();
-        });
-    });
-
-    it("reads from an existing file synchronously", function() {
-        const data = fileIO.read("test/dir1/file1");
-        expect(data).to.equal("This is a test file.");
-    });
-
-    it("returns an error from an asynchronous read of a nonexistant file", function(done) {
-        fileIO.read("test/dir2/file1", function(error, data) {
-            expect(error).to.exist;
-            expect(data).to.not.exist;
-            done();
-        });
-    });
-
-    it("throws an error from a synchronous read of a nonexistant file", function() {
-        expect(fileIO.read.bind(fileIO, "test/dir2/file1")).to.throw(Error);
-    });
-
-    it("overwrites an existing file synchronously", function(done) {
-        fileIO.write("test/dir1/file1", "Different text.");
-        fs.readFile("test/dir1/file1", "utf8", function(error, data) {
-            expect(error).to.be.null;
-            expect(data).to.equal("Different text.");
-            done();
-        });
-    });
-
-    it("overwrites an existing file asynchronously", function(done) {
-        fileIO.write("test/dir1/file1", "Different text.", function(error) {
-            expect(error).to.not.exist;
-            fs.readFile("test/dir1/file1", "utf8", function(err, text) {
+    it("reads from an existing file asynchronously", function() {
+        return fileIO.read("test/dir1/file1")
+            .then(function(data) {
+                expect(data).to.equal("This is a test file.");
+            })
+            .catch(function(err) {
                 expect(err).to.not.exist;
+            });
+        });
+    });
+
+    it("returns an error from an asynchronous read of a nonexistant file", function() {
+        return fileIO.read("test/dir2/file1")
+            .then(function(data) {
+                expect(data).to.not.exist;
+            })
+            .catch(function(err) {
+                expect(err).to.exist;
+            });
+    });
+
+    it("overwrites an existing file asynchronously", function() {
+        return fileIO.write("test/dir1/file1", "Different text.")
+            .then(function() {
+                return fs.readFileAsync("test/dir1/file1", "utf8")
+            })
+            .then(function(text) {
                 expect(text).to.equal("Different text.");
-                done();
-            });
-        });
-    });
-
-    it("writes a new file synchronously", function(done) {
-        fileIO.write("test/dir1/file2", "Some more text.");
-        fs.readFile("test/dir1/file2", "utf8", function(error, data) {
-            expect(error).to.not.exist;
-            expect(data).to.equal("Some more text.");
-            done();
-        });
-    });
-
-    it("writes a new file asynchronously", function(done) {
-        fileIO.write("test/dir1/file2", "Some more text.", function(error) {
-            expect(error).to.not.exist;
-            fs.readFile("test/dir1/file2", "utf8", function(err, text) {
+            })
+            .catch(function(err) {
                 expect(err).to.not.exist;
+            });
+    });
+
+    it("writes a new file asynchronously", function() {
+        return fileIO.write("test/dir1/file2", "Some more text.")
+            .then(function() {
+                return fs.readFileAsync("test/dir1/file2", "utf8");
+            })
+            .then(function(text) {
                 expect(text).to.equal("Some more text.");
-                done();
+            })
+            .catch(function(err) {
+                expect(err).to.not.exist;
             });
-        });
     });
 
-    it("creates a new directory asynchronously", function(done) {
-        fileIO.createDir("test/dir2", function(error)  {
-            expect(error).to.not.exist;
-            fs.access("test/dir2", function(error) {
+    it("creates a new directory asynchronously", function() {
+        return fileIO.createDir("test/dir2")
+            .then(function() {
+                return fs.accessAsync("test/dir2");
+            })
+            .catch(function(err) {
+                expect(err).to.not.exist;
+            });
+    });
+
+    it("creates a new directory implicitly from a path passed to write()", function() {
+        return fileIO.write("test/dir2/file1", "File text.")
+            .then(function() {
+                return fs.accessAsync("test/dir2");
+            })
+            .catch(function(err) {
+                expect(err).to.not.exist;
+            });
+    });
+
+    it("fails silently trying to create an existing directory asynchronously", function() {
+        return fileIO.createDir("test/dir1")
+            .catch(function(err) {
                 expect(error).to.not.exist;
-                done();
             });
-        });
     });
 
-    it("creates a new directory synchronously", function(done) {
-        fileIO.createDir("test/dir2");
-        fs.access("test/dir2", function(error) {
-            expect(error).to.not.exist;
-            done();
-        });
-    });
-
-    it("creates a new directory implicitly from a path passed to write()", function(done) {
-        fileIO.write("test/dir2/file1", "File text.");
-        fs.access("test/dir2", function(error) {
-            expect(error).to.not.exist;
-            done();
-        });
-    });
-
-    it("fails silently trying to create an existing directory asynchronously", function(done) {
-        fileIO.createDir("test/dir1", function(error) {
-            expect(error).to.not.exist;
-            done();
-        });
-    });
-
-    it("fails silently trying to create an existing directory synchronously", function() {
-        expect(fileIO.createDir.bind(fileIO, "test/dir1")).not.to.throw("EEXIST");
-    });
-
-    it("deletes a file synchronously", function(done) {
-        fileIO.delete("test/dir1/file1");
-        fs.access("test/dir1/file1", function(error) {
-            expect(error.code).to.equal("ENOENT");
-            done();
-        });
-    });
-
-    it("deletes a file asynchronously", function(done) {
-        fileIO.delete("test/dir1/file1", function(error) {
-            expect(error).to.not.exist;
-            fs.access("test/dir1/file1", function(err) {
+    it("deletes a file asynchronously", function() {
+        fileIO.delete("test/dir1/file1")
+            .then(function() {
+                return fs.accessAsync('test/dir1/file1');
+            })
+            .catch(function(err) {
                 expect(err.code).to.equal("ENOENT");
-                done();
             });
-        });
     });
 
-    it("deletes a directory synchronously", function(done) {
-        fileIO.delete("test/dir1");
-        fs.access("test/dir1", function(error) {
-            expect(error.code).to.equal("ENOENT");
-            done();
-        });
-    });
-
-    it("deletes a directory asynchronously", function(done) {
-        fileIO.delete("test/dir1", function(error) {
-            expect(error).to.not.exist;
-            fs.access("test/dir1", function(err) {
+    it("deletes a directory asynchronously", function() {
+        fileIO.delete("test/dir1")
+            .then(function() {
+                return fs.accessAsync("test/dir1");
+            })
+            .catch(function(err) {
                 expect(err.code).to.equal("ENOENT");
-                done();
             });
-        });
     });
 
-    it("returns an error trying to asynchronously delete a file that does not exist", function(done) {
-        fileIO.delete("test/dir1/file2", function(error) {
-            expect(error.code).to.equal("ENOENT");
-            done();
-        });
-    });
-
-    it("throws an error trying to synchronously delete a file that does not exist", function() {
-        expect(fileIO.delete.bind(fileIO, "test/dir1/file2")).to.throw("ENOENT");
-    });
+    it("returns an error trying to asynchronously delete a file that does not exist", function() {
+        fileIO.delete("test/dir1/file2")
+            .catch(function(err) {
+                expect(error.code).to.equal("ENOENT");
+            });
 });
