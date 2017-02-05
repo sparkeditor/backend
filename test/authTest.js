16a4aaf8c86fcd76bd19f786be9d13f6c23af222
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2017-02-04 17:46:03 -0700

    Promisify auth, fixing errors

--- test/authTest.js
+++ test/authTest.js
@@ -1,3 +1,4 @@
+const Promise = require("bluebird");
 const fs = require("fs");
 const path = require("path");
 const sqlite3 = require("sqlite3");
@@ -6,200 +7,171 @@ const database = require("../lib/database");
 const projectManager = require("../lib/projectManager");
 const auth = require("../lib/auth");
 
+Promise.promisifyAll(fs);
+
 describe("Auth", function() {
     let db;
 
-    beforeEach(function(done) {
-        db = new sqlite3.Database(path.join(".", "test.db"), function(err) {
-            if (err) {
-                throw err;
-            }
-            database.setDB(path.join(".", "test.db"), function(err) {
+    beforeEach(function() {
+        return new Promise(function (resolve, reject) {
+            db = new sqlite3.Database(path.join(".", "test.db"), function (err) {
                 if (err) {
-                    throw err;
+                    reject(err);
                 }
-                // create the project table
-                database.createTable("project", {
-                    id: {type: "integer", primaryKey: true},
-                    name: {type: "text", unique: true, notNull: true}
-                }, function(err) {
-                    if (err) {
-                        throw err;
-                    }
-                    // create the user table
-                    database.createTable("user", {
-                        id: {type: "integer", primaryKey: true},
-                        username: {type: "text", unique: true, notNull: true},
-                        password: {type: "text", notNull: true}
-                    }, function(err) {
-                        if (err) {
-                            throw err;
-                        }
-                        // create the linking table
-                        database.createTable("user_project", {
-                            user_id: {type: "integer", foreignKey: {user: "id"}},
-                            project_id: {type: "integer", foreignKey: {project: "id"}},
-                            access_level: {type: "text", notNull: true}
-                        }, function(err) {
-                            if (err) {
-                                throw err;
-                            }
-                            // populate the project table
-                            database.insertInto("project", [
-                                {name: "project1"},
-                                {name: "project2"}
-                            ], function(err) {
-                                if (err) {
-                                    throw err;
-                                }
-                                done();
-                            });
-                        });
-                    });
-                });
+                else {
+                    resolve();
+                }
+            })
+        }).then(() => {
+            return database.setDB(path.join(".", "test.db"))
+        }).then(() => {
+            return database.createTable("project", {
+                id: {type: "integer", primaryKey: true},
+                name: {type: "text", unique: true, notNull: true}
             });
+        }).then(() => {
+            return database.createTable("user", {
+                id: {type: "integer", primaryKey: true},
+                username: {type: "text", unique: true, notNull: true},
+                password: {type: "text", notNull: true}
+            });
+        }).then(() => {
+            return database.createTable("user_project", {
+                user_id: {type: "integer", foreignKey: {user: "id"}},
+                project_id: {type: "integer", foreignKey: {project: "id"}},
+                access_level: {type: "text", notNull: true}
+
+            });
+        }).then(() => {
+            return database.insertInto("project", [{name: "project1"}, {name: "project2"}]);
         });
     });
 
-    afterEach(function(done) {
-        fs.unlink(path.join(".", "test.db"), done);
+    afterEach(function() {
+        return fs.unlinkAsync(path.join(".", "test.db"));
     });
 
-    it("adds a new user and hashes the password", function(done) {
-        auth.addUser({username: "user1", password: "password"}, function(err) {
-            expect(err).to.not.exist;
-            database.query("user", {id: 1}, function(err, rows) {
-                expect(err).to.not.exist;
+    it("adds a new user and hashes the password", function() {
+        return auth.addUser({username: "user1", password: "password"})
+            .then(() => database.query("user", {id: 1}))
+            .then((rows) => {
                 expect(rows).to.have.length(1);
                 expect(rows[0].username).to.equal("user1");
                 expect(rows[0].password).to.not.equal("password");
-                done();
+            })
+            .catch((err) => {
+                expect(err).to.not.exist;
             });
-        });
     });
 
-    it("hashes identical passwords with different salts", function(done) {
-        auth.addUser({username: "user1", password: "password"}, function(err) {
-            expect(err).to.not.exist;
-            auth.addUser({username: "user2", password: "password"}, function(err) {
-                expect(err).to.not.exist;
-                database.query("user", {id: 1}, function(err, rows) {
-                    expect(err).to.not.exist;
-                    const user1 = rows[0];
-                    database.query("user", {id: 2}, function(err, rows) {
-                        expect(err).to.not.exist;
-                        const user2 = rows[0];
+    it("hashes identical passwords with different salts", function() {
+        return auth.addUser({username: "user1", password: "password"})
+            .then(() => auth.addUser({username: "user2", password: "password"}))
+            .then(() => database.query("user", {id: 1}))
+            .then((rows) => rows[0])
+            .then((user1) => {
+                return database.query("user", {id: 2})
+                    .then((rows) => rows[0])
+                    .then((user2) => {
                         expect(user1.password).to.not.equal(user2.password);
-                        done();
                     });
-                });
+            })
+            .catch((err) => {
+                expect(err).to.not.exist;
             });
-        });
     });
 
-    it("authenticates a user", function(done) {
-        auth.addUser({username: "user1", password: "password"}, function(err) {
-            expect(err).to.not.exist;
-            auth.authenticate("user1", "password", function(err, isAllowed) {
+    it("authenticates a user", function() {
+        return auth.addUser({username: "user1", password: "password"})
+            .then(() => auth.authenticate("user1", "password"))
+            .then((hasAccess) => {
+                expect(hasAccess).to.be.true;
+            })
+            .then(() => auth.authenticate("user1", "notThePassword"))
+            .then((hasAccess) => {
+                expect(hasAccess).to.be.false;
+            })
+            .catch((err) => {
                 expect(err).to.not.exist;
-                expect(isAllowed).to.be.true;
-                auth.authenticate("user1", "notThePassword", function(err, isAllowed) {
-                    expect(err).to.not.exist;
-                    expect(isAllowed).to.be.false;
-                    done();
-                });
             });
-        });
     });
 
-    it("authenticates a user for a project", function(done) {
-        auth.addUser({username: "user1", password: "password", projects: [
-            {1: auth.accessLevels.ADMIN},
-            {2: auth.accessLevels.CONTRIBUTOR}
-        ]}, function(err) {
-            expect(err).to.not.exist;
-            auth.authenticate("user1", "password", 1, function(err, isAllowed) {
+    it("authenticates a user for a project", function() {
+        return auth.addUser({username: "user1", password: "password", projects: [ {1: auth.accessLevels.ADMIN}, {2: auth.accessLevels.CONTRIBUTOR} ] })
+            .then(() => auth.authenticate("user1", "password", 1))
+            .then((hasAccess) => {
+                expect(hasAccess).to.be.true;
+            })
+            .then(() => auth.authenticate("user1", "password", 2))
+            .then((hasAccess) => {
+                expect(hasAccess).to.be.true;
+            })
+            .catch((err) => {
                 expect(err).to.not.exist;
-                expect(isAllowed).to.be.true;
-                auth.authenticate("user1", "password", 2, function(err, isAllowed) {
-                    expect(err).to.not.exist;
-                    expect(isAllowed).to.be.true;
-                    done();
-                });
             });
-        });
     });
 
-    it("retrieves a user's access level for a project", function(done) {
-        auth.addUser({username: "user1", password: "password", projects: [
-            {1: auth.accessLevels.ADMIN}
-        ]}, function(err) {
-            expect(err).to.not.exist;
-            auth.getAccess("user1", 1, function(err, accessLevel) {
-                expect(err).to.not.exist;
+    it("retrieves a user's access level for a project", function() {
+        return auth.addUser({username: "user1", password: "password", projects: [{1: auth.accessLevels.ADMIN}]})
+            .then(() => auth.getAccess("user1", 1))
+            .then((accessLevel) => {
                 expect(accessLevel).to.equal(auth.accessLevels.ADMIN);
-                done();
-            });
-        });
-    });
-
-    it("gives a user access to a new project", function(done) {
-        auth.addUser({username: "user1", password: "password"}, function(err) {
-            expect(err).to.not.exist;
-            auth.setAccess("user1", 1, auth.accessLevels.ADMIN, function(err) {
+            })
+            .catch((err) => {
                 expect(err).to.not.exist;
-                auth.getAccess("user1", 1, function(err, accessLevel) {
-                    expect(err).to.not.exist;
-                    expect(accessLevel).to.equal(auth.accessLevels.ADMIN);
-                    done();
-                });
             });
-        });
     });
 
-    it("changes a user's access level to a project", function(done) {
-        auth.addUser({username: "user1", password: "password", projects: [{1: auth.accessLevels.CONTRIBUTOR}]}, function(err) {
-            expect(err).to.not.exist;
-            auth.setAccess("user1", 1, auth.accessLevels.ADMIN, function(err) {
+    it("gives a user access to a new project", function() {
+        return auth.addUser({username: "user1", password: "password"})
+            .then(() => auth.setAccess("user1", 1, auth.accessLevels.ADMIN))
+            .then(() => auth.getAccess("user1", 1))
+            .then((accessLevel) => {
+                expect(accessLevel).to.equal(auth.accessLevels.ADMIN);
+            })
+            .catch((err) => {
                 expect(err).to.not.exist;
-                auth.getAccess("user1", 1, function(err, accessLevel) {
-                    expect(err).to.not.exist;
-                    expect(accessLevel).to.equal(auth.accessLevels.ADMIN);
-                    done();
-                });
             });
-        });
     });
 
-    it("revokes a user's access to a project", function(done) {
-        auth.addUser({username: "user1", password: "password", projects: [{1: auth.accessLevels.CONTRIBUTOR}]}, function(err) {
-             expect(err).to.not.exist;
-             auth.revokeAccess("user1", 1, function(err) {
-                 expect(err).to.not.exist;
-                 auth.getAccess("user1", 1, function(err, accessLevel) {
-                     expect(err).to.exist;
-                     expect(accessLevel).to.not.exist;
-                     done();
-                 });
-             });
-        });
-    });
-
-    it("deletes a user", function(done) {
-        auth.addUser({username: "user1", password: "password", projects: [{1: auth.accessLevels.CONTRIBUTOR}]}, function(err) {
-            expect(err).to.not.exist;
-            auth.removeUser("user1", function(err) {
+    it("changes a user's access level to a project", function() {
+        return auth.addUser({username: "user1", password: "password", projects: [{1: auth.accessLevels.CONTRIBUTOR}]})
+            .then(() => auth.setAccess("user1", 1, auth.accessLevels.ADMIN))
+            .then(() => auth.getAccess("user1", 1))
+            .then((accessLevel) => {
+                expect(accessLevel).to.equal(auth.accessLevels.ADMIN);
+            })
+            .catch((err) => {
                 expect(err).to.not.exist;
-                database.query("user", {username: "user1"}, function(err, rows) {
-                    expect(err).to.not.exist;
-                    expect(rows).to.have.length(0);
-                    database.query("user_project", {user_id: 1}, function(err, rows) {
-                        expect(err).to.not.exist;
-                        expect(rows).to.have.length(0);
-                        done();
-                    });
-                });
             });
-        });
+    });
+
+    it("revokes a user's access to a project", function() {
+        return auth.addUser({username: "user1", password: "password", projects: [{1: auth.accessLevels.CONTRIBUTOR}]})
+            .then(() => auth.revokeAccess("user1", 1))
+            .then(() => auth.getAccess("user1", 1))
+            .then((accessLevel) => {
+                expect(accessLevel).to.not.exist;
+            })
+            .catch((err) => {
+                expect(err).to.exist;
+                expect(err.message).to.equal("user1 does not have access to project id 1");
+            });
+    });
+
+    it("deletes a user", function() {
+        return auth.addUser({username: "user1", password: "password", projects: [{1: auth.accessLevels.CONTRIBUTOR}]})
+            .then(() => auth.removeUser("user1"))
+            .then(() => database.query("user", {username: "user1"}))
+            .then((rows) => {
+                expect(rows).to.have.length(0);
+            })
+            .then(() => database.query("user_project", {user_id: 1}))
+            .then((rows) => {
+                expect(rows).to.have.length(0);
+            })
+            .catch((err) => {
+                expect(err).to.not.exist;
+            });
     });
 });
